45cc649dc885007f1db83b5323ef717e04818b64
DiaviceCat_ <168710858+Pollaghan@users.noreply.github.com>
2026-05-30 19:53:41 +0100

    Delete src/main directory

--- src/main/kotlin/com/fref/redstonerouters/Functions.kt
+++
@@ -1,9 +0,0 @@
-package com.fref.redstonerouters
-
-import org.bukkit.Bukkit
-
-object Functions {
-    fun RunCommand(Command: String) {
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), Command);
-    }
-}
